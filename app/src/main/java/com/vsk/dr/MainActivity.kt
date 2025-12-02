a0ef71955ab6f2eb1651bfa21310bc5661f8ab78
Vasily <invweb@gmail.com>
2025-12-02 18:41:29 +0300

    in progress: bugfix

--- app/src/main/java/com/vsk/dr/MainActivity.kt
+++ app/src/main/java/com/vsk/dr/MainActivity.kt
@@ -16,10 +16,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -71,7 +68,6 @@ class MainActivity : ComponentActivity() {
                                             finishAndRemoveTask()
                                         }
                                     }) {
-                                        Icon(Icons.AutoMirrored.Filled.ArrowBack, "")
                                     }
                                 }
                             )
